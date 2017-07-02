cd93abf54619b0809b4371100791b9ba79738ac1
Administrator <Administrator@192.168.0.102>
2017-07-02 17:43:04 +0800

    查错，适配完成SSM框架

--- adoptpet/src/main/java/com/jin2ml/adoptpet/controller/BaseController.java
+++ adoptpet/src/main/java/com/jin2ml/adoptpet/controller/BaseController.java
@@ -1,24 +1,20 @@
 package com.jin2ml.adoptpet.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BaseController
 {  
-    @RequestMapping(name="hello",method=RequestMethod.GET)
-    public String sayHello()
+    @RequestMapping("hello")
+    public ModelAndView handleRequest() throws Exception
     {
-        ModelAndView view = new ModelAndView("hello");
-        view.addObject("name", "111");
+        ModelAndView view = new ModelAndView("hello","name","000");
+        view.addObject("heihei", "heihei");
+        view.getModel().put("names", "names");
         System.out.println("hello world");
-       return "hello";
+       return view;
     }
 
 }
--- adoptpet/src/main/java/com/jin2ml/adoptpet/controller/interceptor/UserAuthInterceptor.java
+++ adoptpet/src/main/java/com/jin2ml/adoptpet/controller/interceptor/UserAuthInterceptor.java
@@ -6,10 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class UserAuthInterceptor extends HandlerInterceptorAdapter
+public class UserAuthInterceptor 
 {
-    public boolean preHandle(HttpServletRequest  request,HttpServletResponse  response,
-            Object handler)
-            throws Exception{
-                return true;}
+    //*public boolean preHandle(HttpServletRequest  request,HttpServletResponse  response,
+       //     Object handler)
+         //   throws Exception{
+           //     return true;}
 }
